2898ac9eab15aae88b474b40b36cb86078dffc0a
Cynthia Parks <cparks@chapman.edu>
2017-10-02 00:53:44 +0000

    more updates

--- cplane.py
+++ cplane.py
@@ -11,14 +11,46 @@ class ListComplexPlane(AbsComplexPlane):
         self.ymin = ymin
         self.ymax = ymax
         self.ylen = ylen
-        self.plane = []
+        self.plane = __create_plane(self.xmin, self.xmax, self.xlen, self.ymin, self.ymax, self.ylen)
         self.fs = []
         
-        dx = (xmax - xmin)/(xlen-1)
-        dy = (ymax - ymin)/(ylen-1)
+    ##creates plane of complex points    
+    def __create_plane(xmin, xmax, xlen, ymin, ymax, ylen)
+        
+        dx = (xmax - xmin)/(xlen - 1)
+        dy = (ymax - ymin)/(ylen - 1)
+        
+        #lists of points on the x and y axis
+        xpoints = []
+        ypoints = []
+        
+        #appends points to the x axis
+        xpoints.append(xmin)
+        
+        while i in (1...xlen):
+            xpoints.append(xmin + i*dx)
+            
+        #appends points to the y axis
+        ypoints.append(ymin)
+    
+        while i in (1...ylen):
+            ypoints.append(ymin + i*dy)
+        
+        
+        
         
     
     def refresh(self):
+        
+        x = self.xmin
+        y = self.ymin
+        xl = self.xlen
+        yl = self.ylen
+        self.plane = []
+        self.fs = []
+        
+        dx = (xmax - x)/(xl-1)
+        dy = (ymax - y)/(yl-1)
         
         
     def apply(self, f):
